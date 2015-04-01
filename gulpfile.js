07dd3fbfe225b6f8148a07004863c8fa04da2513
Thang Kieu <thang.kieu@naustud.io>
2015-04-01 14:55:18 +0700

    add compress

--- gulpfile.js
+++ gulpfile.js
@@ -17,25 +17,46 @@ var dir = {
 
 var path = {
    htmls: {
-      home: dir.src + 'home.html',
-      subpage: dir.src + 'subpage.html',
-      homeMobile: dir.src + 'home_mobile.html',
+      home         : dir.src + 'home.html',
+      subpage      : dir.src + 'subpage.html',
+      homeMobile   : dir.src + 'home_mobile.html',
       subpageMobile: dir.src + 'subpage_mobile.html'
    },
    source: {
-      root: dir.src,
-      imgs: dir.src + 'images/',
-      fonts: dir.src + 'fonts/',
+      root    : dir.src,
+      imgs    : dir.src + 'images/',
+      fonts   : dir.src + 'fonts/',
       homepage: dir.src + 'homepage/',
-      subpage: dir.src + 'subpage/'
+      subpage : dir.src + 'subpage/'
    },
    optimized: {
-      root: dir.optimized,
-      imgs: dir.optimized + 'images/',
-      fonts: dir.optimized + 'fonts/',
+      root    : dir.optimized,
+      imgs    : dir.optimized + 'images/',
+      fonts   : dir.optimized + 'fonts/',
       homepage: dir.optimized + 'homepage/',
-      subpage: dir.optimized + 'subpage/'
+      subpage : dir.optimized + 'subpage/'
    }
+};
+
+var compress = {
+   sequences     : true,  // join consecutive statemets with the “comma operator”
+   properties    : true,  // optimize property access: a["foo"] → a.foo
+   dead_code     : true,  // discard unreachable code
+   drop_debugger : true,  // discard “debugger” statements
+   unsafe        : false, // some unsafe optimizations (see below)
+   conditionals  : true,  // optimize if-s and conditional expressions
+   comparisons   : true,  // optimize comparisons
+   evaluate      : true,  // evaluate constant expressions
+   booleans      : true,  // optimize boolean expressions
+   loops         : true,  // optimize loops
+   unused        : true,  // drop unused variables/functions
+   hoist_funs    : true,  // hoist function declarations
+   hoist_vars    : false, // hoist variable declarations
+   if_return     : true,  // optimize if-s followed by return/continue
+   join_vars     : true,  // join var declarations
+   cascade       : true,  // try to cascade `right` into `left` in sequences
+   side_effects  : true,  // drop side-effect-free statements
+   warnings      : true,  // warn about potentially dangerous optimizations/code
 };
 
 /**
@@ -48,11 +69,17 @@ gulp.task('minify-home', function() {
             advanced: true,
             keepSpecialComments: 0
          }), 'concat'],
-         commonJs: [uglify()],
-         libJs: [uglify()],
-         js: [uglify()]
+         commonJs: [uglify({
+            compress: compress
+         })],
+         libJs: [uglify({
+            compress: compress
+         })],
+         js: [uglify({
+            compress: compress
+         })]
       }))
-      .pipe(gulp.dest(path.optimized.root))
+      .pipe(gulp.dest(path.optimized.root));
 });
 
 /**
@@ -65,11 +92,17 @@ gulp.task('minify-subpage', function() {
             advanced: true,
             keepSpecialComments: 0
          }), 'concat'],
-         commonJs: [uglify()],
-         libJs: [uglify()],
-         js: [uglify()]
+         commonJs: [uglify({
+            compress: compress
+         })],
+         libJs: [uglify({
+            compress: compress
+         })],
+         js: [uglify({
+            compress: compress
+         })]
       }))
-      .pipe(gulp.dest(path.optimized.root))
+      .pipe(gulp.dest(path.optimized.root));
 });
 
 gulp.task('minify-desktop', ['minify-home', 'minify-subpage']);
